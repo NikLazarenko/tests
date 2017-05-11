79a28627410e8452f1a146ae48d1fb4581209ca0
Mykola Lazarenko <m.lazarenko@astoundcommerce.com>
2017-05-11 10:24:47 +0300

    small changes in structure

--- app.js
+++ app.js
@@ -13,33 +13,32 @@ function TextCounter(textField, maxChar, total, left) {
             throw new Error('Please, check your selectors; ' + error);
         }
     };
-
-    TextCounter.prototype.events = function() {
-        this.textArea.addEventListener('keyup', this.textLimiter.bind(this));
-    };
-
-    TextCounter.prototype.textLimiter = function () {
-        if (this.textAreaLength() >= this.maxChar) {
-            this.textArea.value = this.textArea.value.substring(0, this.maxChar);
-        }
-        this.displayLengthCalc();
-    };
-
-    TextCounter.prototype.textAreaLength = function () {
-        return this.textArea.value.length;
-    };
-
-    TextCounter.prototype.displayLengthCalc = function () {
-        this.textAreaTotal.innerHTML = this.textAreaLength();
-        this.textAreaLeft.innerHTML = this.maxChar - this.textAreaLength();
-    };
-
-    TextCounter.prototype.setMaxLength = function () {
-        this.textArea.setAttribute('maxLength', this.maxChar);
-    };
-
     this.init();
 }
+
+TextCounter.prototype.events = function() {
+    this.textArea.addEventListener('keyup', this.textLimiter.bind(this));
+};
+
+TextCounter.prototype.textLimiter = function () {
+    if (this.textAreaLength() >= this.maxChar) {
+        this.textArea.value = this.textArea.value.substring(0, this.maxChar);
+    }
+    this.displayLengthCalc();
+};
+
+TextCounter.prototype.textAreaLength = function () {
+    return this.textArea.value.length;
+};
+
+TextCounter.prototype.displayLengthCalc = function () {
+    this.textAreaTotal.innerHTML = this.textAreaLength();
+    this.textAreaLeft.innerHTML = this.maxChar - this.textAreaLength();
+};
+
+TextCounter.prototype.setMaxLength = function () {
+    this.textArea.setAttribute('maxLength', this.maxChar);
+};
 
 document.addEventListener("DOMContentLoaded", function () {
     var textArea1 = new TextCounter('#js-message', 140, '#js-message-left-total', '#js-message-left-symbols');
